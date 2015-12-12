f48064c7cef0aa49da53620183a6bc3b208a203a
MichalWojcieszyk <mwojcieszyk@gmail.com>
2015-12-12 12:54:42 +0100

    Add achievement Model tests

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Achievement do
+	describe 'database columns' do
+		it { should have_db_column :name }
+		it { should have_db_column :description }
+		it { should have_db_column :image }
+		it { should have_db_column :seen }
+	end
+
+  describe 'associations' do
+  	it { is_expected.to belong_to :achievementable }
+  end
 end
