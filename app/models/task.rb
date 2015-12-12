c407ba17f69571e63b1b98c242af74356bc8231e
kamilpuchala <kamilpuchalaa@gmail.com>
2015-12-12 16:27:52 +0100

    Cron tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,11 @@ class Task < ActiveRecord::Base
   enum state: STATES
 
   belongs_to :habit
+<<<<<<< HEAD
 
   scope :on_day, -> (date) { where(created_at: (date.beginning_of_day)..(date.end_of_day)) }
 
+=======
+  scope :today, { created_at: (Time.now.beginning_of_day..Time.now.end_of_day) }
+>>>>>>> Cron tasks
 end
