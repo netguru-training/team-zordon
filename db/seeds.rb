6b982ced3b4279f3c2c1e6e5679a4c9201809e52
Krzysztof Drab <krabex93@gmail.com>
2015-12-12 14:15:51 +0100

    Added habits, tasks seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,27 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
+#
+
+user = User.create(email: 'admin@admin.com', password: 'adminadmin')
+
+3.times do
+  has_small = rand(1..2) == 1
+
+  Habit.create!(
+    user: user,
+    name: Faker::Lorem.sentence(4),
+    description: Faker::Lorem.paragraph,
+    days: [rand(0..6), rand(0..6)].sort.uniq,
+    small_name: (has_small ? Faker::Lorem.sentence(3) : nil),
+    small_description: (has_small ? Faker::Lorem.paragraph : nil)
+  )
+end
+puts "CREATED HABITS"
+
+Habit.all.each do |habit|
+  (0..10).each do |index|
+    habit.tasks << Task.create(state: rand(0..3), created_at: DateTime.current - index.days, habit: habit)
+  end
+end
+puts "CREATED TASKS"
