d120c4b1447467b72d05afc79705c35faf5acff1
Robert Walerowiccz <robwalerowicz@gmail.com>
2015-12-13 11:59:43 +0100

    Add habit controller configuration

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,11 +1,21 @@
+# controller for habits
 class HabitsController < ApplicationController
   before_action :authenticate_user!
 
   expose(:user_habits) { current_user.habits.includes(:tasks) }
+  expose(:habit, attributes: :habit_params)
+
 
   def index
   end
 
   def history
+  end
+
+  private
+
+  def habit_params
+    params.require(:habit).permit(:name, :descriptino,
+      :small_name, :small_description, :active)
   end
 end
